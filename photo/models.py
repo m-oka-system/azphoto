96d03b4acf59edaa27454334166e96d723f27bb6
m-oka-system <m-oka-system@outlook.com>
2023-05-06 05:03:20 +0000

    [add]Modelを作成

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(verbose_name="画像", upload_to="photos/")
+    title = models.CharField(verbose_name="タイトル", max_length=100, blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title if self.title else f"Photo {self.pk}"

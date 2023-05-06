0772188d1bb011e10c28d4c752ce17d3b1e2a463
m-oka-system <m-oka-system@outlook.com>
2023-05-06 05:53:42 +0000

    [update]ルートをログイン画面に変更

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from allauth.account.views import LoginView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("photo.urls")),
     path("accounts/", include("allauth.urls")),
+    path("", LoginView.as_view(), name="account_login"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- photo/urls.py
+++ photo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from photo.views import PhotoListView, PhotoCreateView, PhotoUpdateView, PhotoDeleteView
 
 urlpatterns = [
-    path("", PhotoListView.as_view(), name="photo_list"),
+    path("photos/", PhotoListView.as_view(), name="photo_list"),
     path("create/", PhotoCreateView.as_view(), name="photo_create"),
     path("<int:pk>/edit/", PhotoUpdateView.as_view(), name="photo_edit"),
     path("delete/<int:pk>/", PhotoDeleteView.as_view(), name="photo_delete"),

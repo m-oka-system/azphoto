2d4f0e36812ff7a83ec9e37b86f53184a5e55310
m-oka-system <m-oka-system@outlook.com>
2023-05-06 05:09:17 +0000

    [add]Viewを作成

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from photo.models import Photo
+
+
+class PhotoListView(ListView):
+    model = Photo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["photo_count"] = Photo.objects.count()
+        return context
+
+
+class PhotoCreateView(CreateView):
+    model = Photo
+    fields = "__all__"
+    success_url = "/"
+
+
+class PhotoUpdateView(UpdateView):
+    model = Photo
+    fields = "__all__"
+    success_url = "/"
+
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = "/"

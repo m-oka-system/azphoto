8a0daf7917f22e5cf58e4e68f7fd50dafba56c94
m-oka-system <m-oka-system@outlook.com>
2023-05-07 08:52:41 +0000

    [add]プライベートエンドポイントを追加(BLOB, KeyVault)

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,35 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-${var.env}-rg"
   location = var.location
 }
+
+module "blob_pep" {
+  source = "./modules/private_endpoint"
+
+  prefix                         = var.prefix
+  env                            = var.env
+  resource_group_name            = azurerm_resource_group.rg.name
+  location                       = azurerm_resource_group.rg.location
+  resource_name                  = azurerm_storage_account.app.name
+  private_connection_resource_id = azurerm_storage_account.app.id
+  virtual_network_id             = azurerm_virtual_network.spoke1.id
+  subnet_id                      = azurerm_subnet.spoke1_endpoint.id
+  subresource_names              = ["blob"]
+  private_dns_host_name          = azurerm_storage_account.app.name
+  private_dns_zone_name          = "privatelink.blob.core.windows.net"
+}
+
+module "vault_pep" {
+  source = "./modules/private_endpoint"
+
+  prefix                         = var.prefix
+  env                            = var.env
+  resource_group_name            = azurerm_resource_group.rg.name
+  location                       = azurerm_resource_group.rg.location
+  resource_name                  = azurerm_key_vault.app.name
+  private_connection_resource_id = azurerm_key_vault.app.id
+  virtual_network_id             = azurerm_virtual_network.spoke1.id
+  subnet_id                      = azurerm_subnet.spoke1_endpoint.id
+  subresource_names              = ["vault"]
+  private_dns_host_name          = azurerm_key_vault.app.name
+  private_dns_zone_name          = "privatelink.vaultcore.azure.net"
+}

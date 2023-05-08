218d008156b7b40530b98318c07bd335dc3620d4
m-oka-system <m-oka-system@outlook.com>
2023-05-08 06:54:44 +0000

    [add]本番環境用のsettigsを追加（DATABASE）

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,3 +14,16 @@ if env("WEBSITE_HOSTNAME"):
 # SECURITY
 CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+
+
+# DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DB_NAME"),
+        "HOST": env("DB_HOST"),
+        "USER": env("DB_USERNAME"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "PORT": env("DB_PORT"),
+    }
+}

1a08cdd67bce8494ff1557abfa58378b213421dd
m-oka-system <m-oka-system@outlook.com>
2023-05-24 11:41:16 +0900

    Revert to 0c1368

--- terraform/infra/variables.tf
+++ terraform/infra/variables.tf
@@ -1,13 +1,5 @@
 # Input Variables
 # Common
-variable "subscription_id" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-
 variable "prefix" {
   type    = string
   default = "azphoto"
